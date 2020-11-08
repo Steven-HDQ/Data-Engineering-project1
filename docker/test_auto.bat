d1b238ce3fbc4fe3447161ed86e00adcb44afb5c
Steven-HDQ <61394921+Steven-HDQ@users.noreply.github.com>
2020-11-08 23:34:42 +0800

    auto run
    auto run

---
+++ docker/test_auto.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:check
+wget -q --spider http://localhost:5000
+if errorlevel 1 goto offline
+if errorlevel 0 goto online
+
+:online
+echo "Online"
+python test_app.py
+pause
+
+:offline
+echo "Offline"
+goto check
